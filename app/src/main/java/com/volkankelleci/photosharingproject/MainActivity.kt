6fb919309b6e240bc6a2e827b8921319b5e352d2
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2022-10-11 11:35:42 +0300

    Initial commit

---
+++ app/src/main/java/com/volkankelleci/photosharingproject/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.volkankelleci.photosharingproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun save(view: View){
+
+    }
+    fun sign (view: View){
+
+    }
+}
